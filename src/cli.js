03aba3a777555a5e0d740a2be8067c50c8976cd2
Elizabeth Ryabinina <lisushar@gmail.com>
2025-02-27 11:55:27 +0300

    intermediate commit

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const cli = () => {
+export const userName = () => {
 const name = readlineSync.question('May I have your name?')
 return console.log(`Hello, ${name}!`);
 }
